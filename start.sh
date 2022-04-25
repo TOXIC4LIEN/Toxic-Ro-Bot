5244cdd528b4c8b22cd043d584185589fca5d4b3
alien <92714616+TOXIC4LIEN@users.noreply.github.com>
2022-04-25 08:22:50 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-https://github.com/TOXIC4LIEN/Toxic-Ro-Bot.git
+  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Toxic-Ro-Bot
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd Toxic-Ro-Bot
+cd /EvaMaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
